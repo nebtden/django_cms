31b775f8c09b79e86065e687a7d7c8f505061b50
simon <nebtden@gmail.com>
2018-07-24 14:19:54 +0800

    更改配置

--- mysite/settings.py
+++ mysite/settings.py
@@ -177,13 +177,12 @@ CMS_PLACEHOLDER_CONF = {}
 
 DATABASES = {
     'default': {
-        'CONN_MAX_AGE': 0,
-        'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': 'localhost',
-        'NAME': 'project.db',
-        'PASSWORD': '',
-        'PORT': '',
-        'USER': ''
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_cms',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
